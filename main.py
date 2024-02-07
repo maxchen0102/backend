d2a6f57e895ff07a05e63593d65580b95b49c768
Chris Chen <chris.chen@ecpay.com.tw>
2024-02-07 10:23:52 +0800

    完成sqlite腳本

--- main.py
+++ main.py
@@ -1,10 +1,53 @@
-from sqlalchemy import  create_engine, ForeignKey,Column,String,Integer,CHAR 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker 
-
+from sqlalchemy import create_engine,ForeignKey ,Column,String,Integer,CHAR 
+#from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.orm import sessionmaker,declarative_base
 
 Base= declarative_base()
 
+class Person (Base):
+    __tablename__ ="people"
+    
+    ssn =Column("ssn",Integer,primary_key=True ) # ssn 是python 裡面的使用名稱 
+    firstname=Column("firstname",String)  # "firstname",是db 裡面的欄位名稱 
+    gender=Column("gender",CHAR)
+    age=Column("age",Integer)
+    
 
-class Person(Base):
-    __tablename__="people"
+# 建立物件的時候，可以產生instance 
+    def __init__(self,ssn,first,gender,age):
+        self.ssn=ssn 
+        self.firstname=first
+        self.gender=gender
+        self.age=age
+    
+#這樣才可以回傳string 給我們做debug 
+    def __repr__(self):
+        
+        return f"({self.ssn}){self.firstname}({self.gender}{self.age})"
+    
+# 建立連線    
+engine=create_engine("sqlite:///mydb2.sqlite",echo=True)
+# 建立table 
+Base.metadata.create_all(bind=engine) 
+
+# (建立資料庫操作橋梁->config要哪個engine )
+Session=sessionmaker(bind=engine)
+
+# 實體化
+session=Session()
+
+person=Person(123,"re",'m',12)
+
+session.add(person)
+session.commit()
+
+p1=Person(1,"chris",'m',12)
+
+p2=Person(23,"max",'m',121)
+
+p3=Person(12,"bob",'m',122)
+
+session.add(p1)
+session.add(p2)
+session.add(p3)
+session.commit()
