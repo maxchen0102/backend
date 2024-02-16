9ab5b3cf32567ccfac50a45e10012ae4aa9fa5da
Chris Chen <chris.chen@ecpay.com.tw>
2024-02-16 18:07:31 +0800

    完成 pyramid 後端主機的建立

--- SQLAlchemy/test.py
+++ SQLAlchemy/test.py
@@ -5,6 +5,7 @@ try :
     url="https://httpbin.org/post"
     url_auth='https://httpbin.org/basic-auth/1111/1111'
     url_delay= "https://httpbin.org/post/delay/5"
+    url_pyramid="http://localhost:6543/hello"
     params ={
         'id':12,
         "name": "chris",
@@ -16,8 +17,9 @@ try :
     headers={
        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
     }
+
     auth=("1111","1111")
-    res = requests.get(url_delay,auth=auth,params=params,json=data,headers=headers,timeout=4)
+    res = requests.get(url_pyramid)
 
     print(res.text)
     print(res.url)
@@ -25,9 +27,5 @@ try :
     #print(res.headers)
 except ConnectionError:
     print('找不到伺服器')
-except HTTPError:
-    print('網頁找不到')
-except Timeout:
-    print('超過時間沒有回應')
-else:
+
     print('沒有發生問題')
