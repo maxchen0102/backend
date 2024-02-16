901a3d4ac6916a367ecfc659465327056abdfd5e
Chris Chen <chris.chen@ecpay.com.tw>
2024-02-16 17:21:17 +0800

    建立pyramid

--- SQLAlchemy/test.py
+++ SQLAlchemy/test.py
@@ -1,7 +1,33 @@
 import requests
-response = requests.get("http://localhost:3001/api/get_test")
-list=response.json()
-#print(response.json())
-print(list[0])
-print(type(list[0]))
-print(list[0]["name"])
+
+from requests import ConnectionError,HTTPError,Timeout
+try : 
+    url="https://httpbin.org/post"
+    url_auth='https://httpbin.org/basic-auth/1111/1111'
+    url_delay= "https://httpbin.org/post/delay/5"
+    params ={
+        'id':12,
+        "name": "chris",
+    }
+    data={
+        "name":"max",
+        "age": 20
+    }
+    headers={
+       'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+    }
+    auth=("1111","1111")
+    res = requests.get(url_delay,auth=auth,params=params,json=data,headers=headers,timeout=4)
+
+    print(res.text)
+    print(res.url)
+    #print(res.json())
+    #print(res.headers)
+except ConnectionError:
+    print('找不到伺服器')
+except HTTPError:
+    print('網頁找不到')
+except Timeout:
+    print('超過時間沒有回應')
+else:
+    print('沒有發生問題')
