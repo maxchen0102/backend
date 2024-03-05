1e62dc449783591da072bad4f52242358cdf2951
Chris Chen <chris.chen@ecpay.com.tw>
2024-03-05 11:29:49 +0800

    no message

--- User/test1.py
+++ User/test1.py
@@ -1,10 +1,2 @@
-people_list = [
-    {"name": "Alice", "age": 30,"status":1},
-    {"name": "Bob", "age": 25,"status":0},
-    {"name": "Charlie", "age": 35 ,"status":1}
-]
 
-# 使用字典推导式将列表转换为字典
-people_dict = {person["name"]: person["age"] for person in people_list if person["status"]}
-
-print(people_dict)
+qwe
