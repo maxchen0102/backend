111a722cfe833458352902088efd8e0baeb25c06
Chris Chen <chris.chen@ecpay.com.tw>
2024-03-08 15:04:57 +0800

    no message

--- myServer/myServer/routes.py
+++
@@ -1,8 +0,0 @@
-def includeme(config):
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.add_route('hello', '/hello')
-    config.add_route('example_route', '/example_route')
-    config.add_route('example_route2', '/example_route2')
-    config.add_route('test_get', '/get')
-    config.add_route('test_post', '/post')
